0d8ad69df18100887791c54297bb37366a1201b2
wanghui <wanghui_zjut@hotmail.com>
2012-12-04 14:51:57 +0800

    Finish game panel background

--- DaDouDou/GamePanel.xaml.cs
+++ DaDouDou/GamePanel.xaml.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,8 +23,13 @@ namespace DaDouDou
     /// </summary>
     public sealed partial class GamePanel : Page
     {
-        private BitmapImage greenImage;
-
+        private const int COLUM_AMOUNT = 25;
+        private const int ROW_AMOUNT = 15;
+        private const int BLOCK_WIDTH = 32;
+        private const int BLOCK_HEIGHT = 32;
+        
+        private double gamePanelStartPointX;
+        private double gamePanleStartPointY;
         public GamePanel()
         {
             this.InitializeComponent();
@@ -37,19 +43,82 @@ namespace DaDouDou
         /// 属性通常用于配置页。</param>
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
-
-            // code add image view
-            Image image = new Image();
-            //BitmapImage bitmapImage = new BitmapImage(new Uri("C:\\Users\\wheight123\\Documents\\Visual Studio 2012\\Projects\\DaDouDou\\DaDouDou\\Assets\\game_page\\bean\\bean10.png", UriKind.Absolute));
-            image.Source = bean1.Source;
-            Thickness myThickness = new Thickness(200, 200, 0, 0);
-            image.Margin = myThickness;
-            gameCanvas.Children.Add(image);
+            // initialize game panel start point x,y coordinate
+            initGamePanelStartPoint();
+            // initialize game panel background
+            initGamePanelBackground();
         }
+
+        private void initGamePanelStartPoint()
+        {
+            CoreWindow cw = CoreWindow.GetForCurrentThread();
+            gamePanelStartPointX = (cw.Bounds.Width - BLOCK_WIDTH * COLUM_AMOUNT) / 2;
+            gamePanleStartPointY = (cw.Bounds.Height - BLOCK_HEIGHT * ROW_AMOUNT) / 2;
+        }
+        private void initGamePanelBackground()
+        {
+            for (int i = 1; i <= ROW_AMOUNT; i++)
+            {
+                for (int j = 1; j <= COLUM_AMOUNT; j++)
+                {
+                    Image image = new Image();
+                    image.Name = "bgImage_" + i + "_" + j;
+                    double x = gamePanelStartPointX + (j - 1) * BLOCK_WIDTH;
+                    double y = gamePanleStartPointY + (i - 1) * BLOCK_HEIGHT;
+                    Thickness myThickness = new Thickness(x, y, 0, 0);
+                    image.Margin = myThickness;
+                    if ((i + j) % 2 == 0)
+                    {
+                        image.Source = block1.Source;
+                    }
+                    else
+                    {
+                        image.Source = block2.Source;
+                    }
+                    image.PointerMoved += bgImage_PointerMoved;
+                    image.PointerExited +=bgImage_PointerExited;
+                    image.Tapped += bgImage_Tapped;
+                    gameCanvas.Children.Add(image);
+                }
+            }
+        }
+
+        private void bgImage_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            Image image = (Image)sender;
+            image.Source = bean1.Source;
+        }
+        // point move across bgImage block
+        private void bgImage_PointerMoved(object sender, PointerRoutedEventArgs e)
+        {
+            Image image = (Image)sender;
+            image.Source = block3.Source;
+        }
+        // point exit from bgImage block
+        private void bgImage_PointerExited(object sender, PointerRoutedEventArgs e)
+        {
+            Image image = (Image)sender;
+            String name = image.Name;
+            String[] array = name.Split(new Char[] { '_' });
+            int i = Int32.Parse(array[1]);
+            int j = Int32.Parse(array[2]);
+            if ((i + j) % 2 == 0)
+            {
+                image.Source = block1.Source;
+            }
+            else
+            {
+                image.Source = block2.Source;
+            }
+        }
+
+        
 
         private void backHome_Click_1(object sender, RoutedEventArgs e)
         {
             this.Frame.Navigate(typeof(MainPage));
         }
+
+        
     }
 }
