4497edd6c6c5dd70f9893b04454cd0bb86fd4c3c
wanghui <wanghui_zjut@hotmail.com>
2012-12-04 16:47:00 +0800

    Modify something.

---
+++ DaDouDou/Game.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaDouDou
+{
+    class Game
+    {
+        private int[,] gameZoneMatrix;
+        private int columAmount;
+        private int rowAmount;
+        private int blockType;
+        private int beanAmount;
+
+        private int score;
+        private double remainTime;
+
+        public Game(int columAmount, int rowAmount, int blockType, int beanAmount)
+        {
+            this.columAmount = columAmount;
+            this.rowAmount = rowAmount;
+            this.blockType = blockType;
+            this.beanAmount = beanAmount;
+
+            gameZoneMatrix = new int[rowAmount, columAmount];
+            initGameZoneMatrix();
+
+            score = 0;
+            remainTime = 150;
+        }
+
+        private void initGameZoneMatrix()
+        {
+            int type = blockType + 10;
+            Random random = new Random();
+            for (int i = 0; i < rowAmount; i++)
+            {
+                for (int j = 0; j < columAmount; j++)
+                {
+                    int value = random.Next() % type;
+                    if (value <= 0 || value > blockType)
+                    {
+                        value = 0;
+                    }
+                    gameZoneMatrix[i, j] = value;
+                }
+            }
+        }
+
+        public int[,] getGameZoneMatrix()
+        {
+            return gameZoneMatrix;
+        }
+
+        public void restart()
+        {
+            initGameZoneMatrix();
+        }
+    }
+}
--- DaDouDou/GamePanel.xaml.cs
+++ DaDouDou/GamePanel.xaml.cs
@@ -23,17 +23,20 @@ namespace DaDouDou
     /// </summary>
     public sealed partial class GamePanel : Page
     {
+        private Game game;
         private const int COLUM_AMOUNT = 25;
         private const int ROW_AMOUNT = 15;
         private const int BLOCK_WIDTH = 32;
         private const int BLOCK_HEIGHT = 32;
+        private const int BLOCK_TYPE = 10;
+        private const int BEAN_AMOUNT = 200;
         
         private double gamePanelStartPointX;
         private double gamePanleStartPointY;
         public GamePanel()
         {
             this.InitializeComponent();
-            //greenImage = new BitmapImage(new Uri("Assets/game_page/green_bean.png", UriKind.Relative));
+            game = new Game(COLUM_AMOUNT, ROW_AMOUNT, BLOCK_TYPE, BEAN_AMOUNT);
         }
 
         /// <summary>
@@ -47,49 +50,54 @@ namespace DaDouDou
             initGamePanelStartPoint();
             // initialize game panel background
             initGamePanelBackground();
+            // initialzie game panel
+            initGamePanel();
         }
 
+        // initialize game panel start point x,y coordinate
         private void initGamePanelStartPoint()
         {
             CoreWindow cw = CoreWindow.GetForCurrentThread();
             gamePanelStartPointX = (cw.Bounds.Width - BLOCK_WIDTH * COLUM_AMOUNT) / 2;
             gamePanleStartPointY = (cw.Bounds.Height - BLOCK_HEIGHT * ROW_AMOUNT) / 2;
         }
+
+        // initialize game panel background
         private void initGamePanelBackground()
         {
-            for (int i = 1; i <= ROW_AMOUNT; i++)
+            for (int i = 0; i < ROW_AMOUNT; i++)
             {
-                for (int j = 1; j <= COLUM_AMOUNT; j++)
+                double y = gamePanleStartPointY + i * BLOCK_HEIGHT;
+                for (int j = 0; j < COLUM_AMOUNT; j++)
                 {
                     Image image = new Image();
                     image.Name = "bgImage_" + i + "_" + j;
-                    double x = gamePanelStartPointX + (j - 1) * BLOCK_WIDTH;
-                    double y = gamePanleStartPointY + (i - 1) * BLOCK_HEIGHT;
+                    double x = gamePanelStartPointX + j * BLOCK_WIDTH;
                     Thickness myThickness = new Thickness(x, y, 0, 0);
                     image.Margin = myThickness;
-                    if ((i + j) % 2 == 0)
-                    {
-                        image.Source = block1.Source;
-                    }
-                    else
-                    {
-                        image.Source = block2.Source;
-                    }
-                    image.PointerMoved += bgImage_PointerMoved;
-                    image.PointerExited +=bgImage_PointerExited;
+                    setBackgroundImageSource(image, i, j);
+                    image.PointerEntered += bgImage_PointerEntered;
+                    //image.PointerMoved += bgImage_PointerMoved;
+                    image.PointerExited += bgImage_PointerExited;
                     image.Tapped += bgImage_Tapped;
                     gameCanvas.Children.Add(image);
                 }
             }
         }
-
+        // bgImage block be tapped
         private void bgImage_Tapped(object sender, TappedRoutedEventArgs e)
         {
             Image image = (Image)sender;
-            image.Source = bean1.Source;
+            image.Source = pathPoint.Source;
         }
         // point move across bgImage block
         private void bgImage_PointerMoved(object sender, PointerRoutedEventArgs e)
+        {
+            Image image = (Image)sender;
+            image.Source = block3.Source;
+        }
+        // point enter into bgImage block
+        private void bgImage_PointerEntered(object sender, PointerRoutedEventArgs e)
         {
             Image image = (Image)sender;
             image.Source = block3.Source;
@@ -102,16 +110,85 @@ namespace DaDouDou
             String[] array = name.Split(new Char[] { '_' });
             int i = Int32.Parse(array[1]);
             int j = Int32.Parse(array[2]);
-            if ((i + j) % 2 == 0)
+            setBackgroundImageSource(image, i, j);
+        }
+        // set background image's source
+        private void setBackgroundImageSource(Image image, int i, int j)
+        {
+            int type = (i + j) % 2;
+            switch (type)
             {
-                image.Source = block1.Source;
-            }
-            else
-            {
-                image.Source = block2.Source;
+                case 0:
+                    image.Source = block1.Source;
+                    break;
+                case 1:
+                    image.Source = block2.Source;
+                    break;
+                default:
+                    break;
             }
         }
 
+        // initialize game panel
+        private void initGamePanel()
+        {
+            int[,] gameZoneMatrix = game.getGameZoneMatrix();
+            for (int i = 0; i < ROW_AMOUNT; i++)
+            {
+                double y = gamePanleStartPointY + i * BLOCK_HEIGHT;
+                for (int j = 0; j < COLUM_AMOUNT; j++)
+                {
+                    int type = gameZoneMatrix[i, j];
+                    if (type> 0 && type <= BLOCK_TYPE)
+                    {
+                        Image image = new Image();
+                        double x = gamePanelStartPointX + j * BLOCK_WIDTH;
+                        Thickness myThickness = new Thickness(x, y, 0, 0);
+                        image.Margin = myThickness;
+                        setBeanImageSource(image, type);
+                        gameCanvas.Children.Add(image);
+                    }
+                }
+            }
+        }
+        private void setBeanImageSource(Image image, int type)
+        {
+            switch(type) 
+            {
+                case 1:
+                    image.Source = bean1.Source;
+                    break;
+                case 2:
+                    image.Source = bean2.Source;
+                    break;
+                case 3:
+                    image.Source = bean3.Source;
+                    break;
+                case 4:
+                    image.Source = bean4.Source;
+                    break;
+                case 5:
+                    image.Source = bean5.Source;
+                    break;
+                case 6:
+                    image.Source = bean6.Source;
+                    break;
+                case 7:
+                    image.Source = bean7.Source;
+                    break;
+                case 8:
+                    image.Source = bean8.Source;
+                    break;
+                case 9:
+                    image.Source = bean9.Source;
+                    break;
+                case 10:
+                    image.Source = bean10.Source;
+                    break;
+                default:
+                    break;
+            }
+        }
         
 
         private void backHome_Click_1(object sender, RoutedEventArgs e)
@@ -119,6 +196,11 @@ namespace DaDouDou
             this.Frame.Navigate(typeof(MainPage));
         }
 
-        
+        private void restart_Click(object sender, RoutedEventArgs e)
+        {
+            game.restart();
+            //initGamePanelBackground();
+            initGamePanel();
+        }
     }
 }
